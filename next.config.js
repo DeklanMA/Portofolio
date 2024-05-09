448689992042d9a59ec2a4fcdd3b84616b2c220f
DeklanMA <deklanmalik@gmail.com>
2024-05-09 23:18:27 +0700

    feat:landing page web profile

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ['jrgarciadev.s3.amazonaws.com'],
+  },
+};
---
+++ src/config/index.js
@@ -0,0 +1,62 @@
+/* eslint-disable max-len */
+import featuredProjects from './featured';
+import projects from './projects';
+
+module.exports = {
+  email: 'deklanmalik@gmail.com ',
+  featuredProjects,
+  projects,
+  skills: [
+    'JavaScript',
+    'TypeScript',
+    'PHP',
+    'Python',
+    'React Native',
+    'React',
+    'Laravel',
+    'Express.js',
+    'Next.js',
+    'Node.js',
+    'mySQL',
+    'MongoDB',
+    'GraphQL',
+  ],
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/DeklanMA',
+    },
+    {
+      name: 'Linkedin',
+      url: 'www.linkedin.com/in/deklanma',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/deklanmalik/',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Blog',
+      url: '/#projects',
+    },
+    {
+      name: 'Work',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+};
