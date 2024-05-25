773f7af19df030b2017405e0e4c670ba27958d1f
DeklanMA <deklanmalik@gmail.com>
2024-05-25 20:23:32 +0700

    feat:meta data

--- src/layouts/base.js
+++ src/layouts/base.js
@@ -6,7 +6,7 @@ const BaseLayout = ({ children }) => {
   return (
     <div id="main">
       <Head>
-        <title>Junior García | Web & Mobile developer</title>
+        <title>Deklan Malik Akbar | Web & Mobile developer</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
@@ -30,18 +30,19 @@ const BaseLayout = ({ children }) => {
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@jrgarciadev" />
-        <meta property="og:site_name" content="Junior García | Web & Mobile developer" />
+        <meta property="og:site_name" content="Deklan Malik Akbar | Web & Mobile developer" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Junior García | Web & Mobile developer" />
+        <meta property="og:title" content="Deklan Malik Akbar | Web & Mobile developer" />
         <meta property="og:locale" content="en" />
         <meta property="og:url" content="https://jrgarciadev.com" />
         <meta
           name="description"
-          content="Hello! I'm Junior, a Software Developer based in Buenos Aires, Argentina. I enjoy creating beautiful and reliable applications for internet and phones. My goal is to always build scalable products and performant experiences."
+          content="Hello!  I'm a software developer based in West Java, specializing in building amazing websites
+      and mobile apps, and everything in between."
         />
         <meta
           name="keywords"
-          content="Developer, Javascript, Freelancer, React, React native, PHP, Laravel Developer, Vue JS Developer, React Developer"
+          content="Developer, Javascript, Freelancer, React, React native, PHP, Laravel Developer, Vue JS Developer, React Developer,Typescript"
         />
         <meta property="og:image" content="https://jrgarciadev.com/avatar.jpeg" />
         <meta property="twitter:image" content="https://jrgarciadev.com/avatar.jpeg" />
