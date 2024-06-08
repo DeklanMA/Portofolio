cccc4a159c2eff60a1c40cfad19548712ccbd4da
DeklanMA <deklanmalik@gmail.com>
2024-06-08 14:41:15 +0700

    add meta

--- src/layouts/base.js
+++ src/layouts/base.js
@@ -28,12 +28,11 @@ const BaseLayout = ({ children }) => {
           key="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        {/* <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@jrgarciadev" /> */}
+
         <meta property="og:site_name" content="Deklan Malik Akbar | Web & Mobile developer" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Deklan Malik Akbar | Web & Mobile developer" />
-        <meta property="og:locale" content="en" />
+        <meta property="og:locale" content="id" />
         <meta property="og:url" content="https://deklanma.vercel.app/" />
         <meta
           name="description"
@@ -44,8 +43,14 @@ const BaseLayout = ({ children }) => {
           name="keywords"
           content="Developer, Javascript, Freelancer, React, React native, PHP, Laravel Developer, Vue JS Developer, React Developer,Typescript"
         />
-        <meta property="og:image" content="/public/deklan.webp" />
-        <meta property="twitter:image" content="/public/deklan.webp" />
+        <meta
+          property="og:image"
+          content="https://cdn.discordapp.com/attachments/1092021496542666833/1248903346501193828/152021056-DeklanMalikAkbar.jpeg?ex=66655b61&is=666409e1&hm=95cf1d0f789639efb140ce0625c0c97ab6faf49b1a5de4cadab5d917870b5bd4&"
+        />
+        <meta
+          property="twitter:image"
+          content="https://cdn.discordapp.com/attachments/1092021496542666833/1248903346501193828/152021056-DeklanMalikAkbar.jpeg?ex=66655b61&is=666409e1&hm=95cf1d0f789639efb140ce0625c0c97ab6faf49b1a5de4cadab5d917870b5bd4&"
+        />
       </Head>
       {children}
     </div>
